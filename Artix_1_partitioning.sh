f66eeeef5a3edc8115a7f5b81a4d23a29edeab46
Fabk02 <74472871+Fabk02@users.noreply.github.com>
2024-03-27 18:04:29 +0100

    Add files via upload

---
+++ Artix_1_partitioning.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pacman -Sy parted
+parted /dev/sda --script mklabel gpt
+parted -a optimal /dev/sda --script mkpart "EFI" fat32 1MiB 512MiB
+parted -a optimal /dev/sda --script mkpart "swap" linux-swap 512MiB 2560MiB
+parted -a optimal /dev/sda --script mkpart "root" ext4 2560MiB 100%
