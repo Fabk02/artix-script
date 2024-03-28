2cf17c0098ce8ac35eb22a4e0b6a2ca72083b4b9
Fabk02 <74472871+Fabk02@users.noreply.github.com>
2024-03-28 17:03:33 +0100

    added noconfirm

--- Artix_5_final_installation.sh
+++ Artix_5_final_installation.sh
@@ -2,6 +2,6 @@ echo "artix" >> /etc/hostname
 echo "127.0.0.1     localhost" >> /etc/hosts
 echo "::1           localhost" >> /etc/hosts
 echo "127.0.1.1     artix.localdomain artix" >> /etc/hosts
-pacman -S dhcpcd wpa_supplicant connman-runit
+pacman -S --noconfirm dhcpcd wpa_supplicant connman-runit
 ln -s /etc/runit/sv/connmand /etc/runit/runsvdir/default
 echo "SYSTEM READY, PLEASE EXIT CHROOT AND REBOOT THE SYSTEM"
