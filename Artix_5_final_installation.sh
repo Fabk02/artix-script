b71c35e537c4df0847b18cc24cd4b949193a2186
Fabk02 <74472871+Fabk02@users.noreply.github.com>
2024-03-28 16:04:43 +0100

    typo

--- Artix_5_final_installation.sh
+++ Artix_5_final_installation.sh
@@ -3,7 +3,7 @@ echo "127.0.0.1     localhost" >> /etc/hosts
 echo "::1           localhost" >> /etc/hosts
 echo "127.0.1.1     artix.localdomain artix" >> /etc/hosts
 pacman -S --noconfirm dhcpcd wpa_supplicant connman-runit
-ln -s /etc/runit/sv/connmand /etc/runit/runsvidir/default
+ln -s /etc/runit/sv/connmand /etc/runit/runsvdir/default
 exit
 umount -R /mnt
 reboot
