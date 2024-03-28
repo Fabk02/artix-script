26d8c65a5318d53eca9c6381cb155312edd0f5a5
Fabk02 <74472871+Fabk02@users.noreply.github.com>
2024-03-28 13:24:11 +0100

    Remove space in line 6

--- Artix_1_partitioning.sh
+++ Artix_1_partitioning.sh
@@ -3,4 +3,4 @@ parted -s /dev/sda mkpart "EFI" fat32 1MiB 513MiB
 parted -s /dev/sda set 1 esp on
 parted -s /dev/sda mkpart "SWAP" 513MiB 2560MiB
 parted -s /dev/sda set 2 swap on
-parted -s /dev/sda mkpart "ROOT" ext4 2560 MiB 100%
+parted -s /dev/sda mkpart "ROOT" ext4 2560MiB 100%
